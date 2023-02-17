7ab70c443b2283974c306f3c2c85cca5ab392da7
cody <adwingsx@aim.com>
2023-02-17 17:17:58 -0500

    linked list functions runable

---
+++ README.md
@@ -0,0 +1,9 @@
+# Link Scrapper
+
+## Installing
+
+run npm i
+
+## using puppeteer
+
+Included in this project is a web scraper that uses puppeteer to scrape a website's links. Puppeteer uses a headless browser so, its usage will be limited in a WSL environment. Chromium would need to be
--- usingHTTP.js
+++ usingHTTP.js
@@ -2,10 +2,13 @@ import http from "http";
 import https from "https";
 import fs from "fs";
 
-const options = new URL("https://www.asurascans.com");
+const options = new URL(
+	"https://iapps.courts.state.ny.us/webcivil/ecourtsMain"
+);
 options.headers = {
 	"User-Agent": "Custom User Agent String",
 };
+
 console.log(options);
 
 const req = https.request(options, (res) => {
@@ -13,15 +16,40 @@ const req = https.request(options, (res) => {
 	// console.log(res);
 	let data = "";
 	res.on("data", (chunk) => {
-		console.log(">>>>>>>>>>>>>>");
-		console.log(chunk);
 		data += chunk;
 	});
 	res.on("end", () => {
-		fs.writeFile(`${options.host}.html`, data, (err) => {
-			if (err) throw err;
-			console.log("File saved!");
+		console.log(typeof data);
+		console.log(data.length);
+		const linkRagRegex = /<a.+>/gi;
+		console.log("regex", linkRagRegex);
+		let linkList = data.match(linkRagRegex);
+		console.log("control");
+		linkList = linkList.map((e) => {
+			let splitLink = e.split(/['"]/);
+			// console.log(splitLink);
+			for (let i = 0; i < splitLink.length; i++) {
+				if (/href=/i.test(splitLink[i])) {
+					let resultLink = splitLink[i + 1];
+					// console.log(resultLink);
+					if (/^\w+[:]/i.test(resultLink)) return resultLink;
+					if (resultLink[0] === "/") {
+						resultLink = options.protocol + "//" + options.host + resultLink;
+					} else if (resultLink[0] === "#") {
+						resultLink = options.href + resultLink;
+					} else {
+						let removeSlug = options.href.match(/.+[/]/);
+						resultLink = removeSlug + "/" + resultLink;
+					}
+					return resultLink;
+				}
+			}
 		});
+		console.log("linkList", linkList);
+		// fs.writeFile(`${options.host}.html`, data, (err) => {
+		// 	if (err) throw err;
+		// 	console.log("File saved!");
+		// });
 	});
 });
 req.on("error", (error) => {
